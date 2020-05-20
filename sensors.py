749eb8631bba30547ac82e1751ffca5c4b7b66cc
Dan Duta <danduta23@gmail.com>
2020-05-20 19:31:39 +0300

    grafana script for sensors

--- sensors.py
+++ sensors.py
@@ -2,6 +2,7 @@
 import Adafruit_DHT
 import time
 import os
+from influxdb import InfluxDBClient
 from subprocess import PIPE, Popen
 from optparse import OptionParser
 
@@ -13,8 +14,32 @@ parser.add_option("-q", "--quiet", action="store_true", dest="verbose", default=
 sensor = Adafruit_DHT.DHT22
 pin = 4
 
+client = InfluxDBClient(host='localhost', port=8086, database='temphum')
+
+json_temperature =  [{
+                         "measurement":"temperatura",
+                         "fields": {
+                             "temp_dormitor": 1.0
+                         }
+                     }]
+
+json_humidity =  [{
+                         "measurement":"umiditate",
+                         "fields": {
+                             "hum_dormitor": 1.0
+                         }
+                     }]
+
+json_cputemp =  [{
+                         "measurement":"cputemp",
+                         "fields": {
+                             "temp_cpu": 1.0
+                         }
+                     }]
+
 
 f = open('/home/pi/humidity.csv', 'a+')
+
 if os.stat('/home/pi/humidity.csv').st_size == 0:
     f.write('Date,Time,Temperature,Humidity,CPU_Temp\r\n')
 f.close()
@@ -31,12 +56,24 @@ while True:
             if options.verbose is False:
                 print("Temp={0:0.1f}*C\tHumidity={1:0.1f}%\t\tCPU Temp={2:0.1f}*C".format(temperature, humidity, cpu_tempC))
             f = open('/home/pi/humidity.csv', 'a+')
+            
             time.sleep(1)
+            
             f.write('{0},{1},{2:0.1f}*C,{3:0.1f}%,{4:0.1f}\r\n'.format(time.strftime('%d/%m/%y'),
                 time.strftime('%H:%M'), temperature, humidity, cpu_tempC))
+
+            json_temperature[0]["fields"]["temp_dormitor"] = float(temperature)
+            json_humidity[0]["fields"]["hum_dormitor"] = float(humidity)
+            json_cputemp[0]["fields"]["temp_cpu"] = float(cpu_tempC)
+
+            client.write_points(json_temperature)
+            client.write_points(json_humidity)
+            client.write_points(json_cputemp)
+            print(temperature, humidity, cpu_tempC)
+
             time.sleep(1)
             f.close()
-            time.sleep(900)
+            time.sleep(5)
         else:
             if options.verbose is False:
                 print("Failed to retrieve data from sensor")
@@ -45,5 +82,6 @@ while True:
         if f.closed is False: 
             f.write("Sensor monitoring encountered exception. Closing.")
             f.close()
-        if tFile.close is False:
+        if tFile.closed is False:
             tFile.close()
+        time.sleep(10)
